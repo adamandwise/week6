5faa80113327faa47a90595f6cbdac29a5e494a2
Adam Wise <wise.adam@student.greenriver.edu>
2023-02-07 17:19:31 -0500

    using loops and arrays anmd templates

--- index.php
+++ index.php
@@ -10,7 +10,22 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 //define a default route(328/application) with anonymous function
-$f3 -> route('GET /', function(){
+$f3 -> route('GET /', function($f3){
+
+    //add key-value pairs to the F3 "hive"
+    $f3->set('username','jshmo');
+    $f3->set('password',sha1('jshmo'));
+    $f3->set('title','Working with Templates');
+    $f3->set('favFood','Hotdogs');
+    $f3->set('favColor','purple');
+    $f3->set('favSeason','fall');
+    $f3->set('temp',40);
+    $f3->set('radius',10);
+
+    $fruits = array("apple","banana","orange");
+    $f3->set('fruits',$fruits);
+
+    $f3->set('colors',array("red","blue","green"));
 
 
     //instantiate a view
